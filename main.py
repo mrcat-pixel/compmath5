8326767c71ab6b2d77de54bb985e7c8c1abe91c3
mrcat-pixel <mrcat.pixel@gmail.com>
2022-04-20 12:21:09 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,128 @@
+import math
+import re
+import matplotlib.pyplot as plt
+import numpy as np
+
+LINE = "-----------------------------------------"
+def frm(num): return "{:.4f}".format(num)
+def print_err(): print("Incorrect command. To see the list of commands, type \"h\".")
+
+
+def get_func_description(id, coef):
+    if      id == 0:    return "y' = sin(x + (" + coef + ")) / y^2"
+    elif    id == 1:    return "y' = x^2 - (" + coef + ") * y"
+    else:               return "y' = 0.00001 * (" + coef + ") * xy"
+
+
+def ret_functions(inp):
+    id, coef = int(inp.split()[1]), float(inp.split()[2])
+
+    if id == 0:
+        f = lambda x, y:    math.sin(x + coef) / y**2
+        s = lambda x, c:    np.cbrt(-3 * math.cos(x + coef) + c)
+        c = lambda x, y:    y**3 + 3 * math.cos(x + coef)
+    elif id == 1:
+        f = lambda x, y:    x**2 - coef*y
+        s = lambda x, c:    x**2/coef - 2*(coef*x - 1)/coef**3 + c/(math.e**(coef*x))
+        c = lambda x, y:    (y - x**2/coef + 2*(coef*x - 1)/coef**3) * math.e**(coef*x)
+    else:
+        f = lambda x, y:    0.00001*coef*x*y
+        s = lambda x, c:    math.e**(0.00001*coef*x**2/2 + c)
+        c = lambda x, y:    math.log(y) - 0.00001*coef*x**2/2
+    
+    print("The entered function is: \n" + get_func_description(id, frm(coef)))
+    return f, s, c
+
+
+def print_help():
+    print(  LINE + "\n"
+            "Command list:\n\n"
+            "eq [id] [coef] -- choose a differential equation:")
+    for i in range(3):
+        print("     " + str(i) + ": " + get_func_description(i, "[coef]"))
+    print(  "p [x0] [x1] [y] [n] -- set the parameters for solving;\n\n"
+            "calc -- calculate;\n\n"
+            "h -- display this message;\n"
+            "q -- quit\n" +
+            LINE)
+
+
+def split_args(inp):
+    x0, x1, y0, n = \
+        float(inp.split()[1]), float(inp.split()[2]), float(inp.split()[3]), int(inp.split()[4])
+    print("x0 = " + frm(x0) + "; x1 = " + frm(x1) + "; y0 = " + frm(y0) + "; n = " + str(n) + ".")
+
+    return x0, x1, y0, n
+
+
+def plot_solution(x0, x1, s, c):
+    x = np.linspace(x0, x1, 1000)
+    y = np.array([s(xi, c) for xi in x])
+
+    plt.plot(x, y, 'k:', label="Analytic solution")
+
+
+def plot_points(points):
+    x = np.array(points[0])
+    y = np.array(points[1])
+
+    plt.plot(x, y, 'g', label="Euler method")
+
+
+def plot_show():
+    plt.title("Differential equation solution")
+    plt.legend(loc='upper left')
+
+    plt.show()
+
+
+def method(x0, x1, y0, n, f):
+    xarr, yarr = [], []
+    y, x, step = y0, x0, (x1-x0)/n
+    for i in range(n):
+        x       += step
+        y       += step * f(x, y)
+        xarr    += [x]
+        yarr    += [y]
+    
+    return (xarr, yarr)
+
+
+def calc(x0, x1, y0, n, f, s, c):
+    try:
+        plot_points(method(x0, x1, y0, n, f))
+        plot_solution(x0, x1, s, c(x0, y0))
+        plot_show()
+
+    except ZeroDivisionError:
+        print(  "Encountered a division by zero while trying to calculate the solution.\n"
+                "Please use different arguments.")
+    except OverflowError:
+        print(  "Encountered an overflow error while trying to calculate the solution.\n"
+                "Please use different arguments.")
+    
+
+def prompt():
+    x0, x1, y0, n = -10, 10, 1, 1000
+    f             = lambda x, y: 0
+    s             = lambda x, c: c
+    c             = lambda x, y: y
+    while True:
+        
+        try:                inp = input("> ")
+        except EOFError:    break
+
+        if re.match(r"^eq [0-2] -?\d+.?\d*$", inp):
+            f, s, c = ret_functions(inp)
+        elif re.match(r"^p -?\d+.?\d* -?\d+.?\d* -?\d+.?\d* \d+$", inp):
+            x0, x1, y0, n = split_args(inp)
+        
+        elif inp == "calc": calc(x0, x1, y0, n, f, s, c)
+        elif inp == "h":    print_help()
+        elif inp == "q":    break
+        else:               print_err()
+
+
+print(  "Welcome to the differential equation calculator.\n" 
+        "To see the list of commands, type \"h\". To quit, type \"q\".\n" + LINE)
+prompt()
